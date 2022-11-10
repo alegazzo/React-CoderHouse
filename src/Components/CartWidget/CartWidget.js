7d83caf9682ecf7299ceb369f42b4af70338471c
alegazzo <72534448+alegazzo@users.noreply.github.com>
2022-11-09 22:29:06 -0300

    primer Commit Proyecto final preentrega

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,6 @@
+export default function CartWidget(){
+    return(
+        
+    );
+
+}
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+export default function NavBar(){
+    return(
+        <div className="navBar">
+        <img></img>    
+        <nav>
+            <ul>
+                <li><a>Armas</a></li>
+                <li><a>Trinkets</a></li>
+                <li><a>Anillos</a></li>
+                <li><a>Set</a></li>                
+            </ul>
+        </nav>
+
+        </div>
+    );
+
+}
